08bd8ab97e74af1db38a39a154c9f045ea5c2ffd
Tushar Kukwase <111518709+Tushakukwase@users.noreply.github.com>
2024-12-15 16:16:55 +0530

    app.py

--- app.py
+++ app.py
@@ -2,29 +2,6 @@ import cv2
 import time
 from flask import Flask, render_template, Response
 
-# face_cap = cv2.CascadeClassifier("D:/python projects/env/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml")
-# video_cap =cv2.VideoCapture(0)
-# while True :
-#     ret , video_data = video_cap.read()
-#     col = cv2.cvtColor(video_data,cv2.COLOR_BGR2GRAY)
-#     faces = face_cap.detectMultiScale(
-#         col,
-#         scaleFactor=1.1,
-#         minNeighbors=5,
-#         minSize=(30, 30),
-#         flags=cv2.CASCADE_SCALE_IMAGE
-#     )
-#     for (x,y,w,h) in faces:
-#         cv2.rectangle(video_data,(x,y),(x+w,y+h),(0,255,0),2)
-#     cv2.imshow("video_live",video_data)
-#     if cv2.waitKey(10) == ord("a"):
-#         break
-# video_cap.release()
-
-
-
-
-
 app = Flask(__name__)
 
 # Load the face detection model
@@ -70,25 +47,3 @@ def video_feed():
 # Start the Flask app
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-# face_cap = cv2.CascadeClassifier("D:\python projects\env\Lib\site-packages\cv2\data\haarcascade_eye.xml")
-# video_cap =cv2.VideoCapture(0)
-# while True :
-#     ret , video_data = video_cap.read()
-#     col = cv2.cvtColor(video_data,cv2.COLOR_BGR2GRAY)
-#     faces = face_cap.detectMultiScale(
-#         col,
-#         scaleFactor=1.1,
-#         minNeighbors=5,
-#         minSize=(30, 30),
-#         flags=cv2.CASCADE_SCALE_IMAGE
-#     )
-#     for (x,y,w,h) in faces:
-#         cv2.rectangle(video_data,(x,y),(x+w,y+h),(0,255,0),2)
-#     cv2.imshow("video_live",video_data)
-#     if cv2.waitKey(10) == ord("a"):
-#         break
-# video_cap.release()
